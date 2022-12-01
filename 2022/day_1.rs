5462166485f6ad0ede09ca597d9e801da22fa251
John Sanchez <johnmarion1126@gmail.com>
2022-12-01 22:41:46 +0000

    Add solution for day 1 part 2

--- 2022/day_1.rs
+++ 2022/day_1.rs
@@ -16,3 +16,27 @@ fn find_elves_with_most_calories(input: String) -> i32 {
 
     max
 }
+
+fn find_elves_with_most_calories2(input: String) -> i32 {
+    let mut total = 0;
+    let mut top_three: Vec<i32> = vec!(0, 0, 0);
+
+    for i in input.split("\n") {
+        match i.trim().parse::<i32>() {
+            Ok(x) => total += x,
+            Err(_) => {
+                for (index, value) in top_three.iter().enumerate() {
+                    if value < &total {
+                        top_three.insert(index, total);
+                        top_three.pop();
+                        break;
+                    }
+                }
+
+                total = 0;
+            }
+        }
+    }
+
+    top_three.iter().sum()
+}
