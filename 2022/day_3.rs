ebc196436d1ff280d517279db446d5e93dad3e56
John Sanchez <johnmarion1126@gmail.com>
2022-12-03 21:05:53 +0000

    Add solution to day 3 part 2

--- 2022/day_3.rs
+++ 2022/day_3.rs
@@ -39,3 +39,51 @@ fn calculate_sum(input: String) -> i32 {
     }
     total
 }
+
+//================================================================================================
+
+use std::collections::HashSet;
+
+fn map_to_score(val: char) -> i32 {
+    if val.is_lowercase() {
+        return ((val as u32) - 96) as i32;
+    }
+
+    ((val as u32) - 38) as i32
+}
+
+fn find_common_char(lines: Vec<String>) -> Option<char> {
+    let mut first_set: HashSet<char> = HashSet::new();
+    let mut second_set: HashSet<char> = HashSet::new();
+
+    for i in lines[0].chars() {
+        if !first_set.contains(&i) {
+            first_set.insert(i);
+        }
+    }
+
+    for i in lines[1].chars() {
+        if first_set.contains(&i) {
+            second_set.insert(i);
+        }
+    }
+
+    lines[2].chars().find(|&i| second_set.contains(&i))
+}
+
+fn calculate_sum(input: String) -> i32 {
+    let mut total = 0;
+    let mut lines: Vec<String> = Vec::new();
+
+    for i in input.lines() {
+        lines.push(i.trim().to_string());
+        if lines.len() == 3 {
+            if let Some(x) = find_common_char(lines) {
+                total += map_to_score(x);
+            }
+            lines = Vec::new();
+        }
+    }
+    total
+}
+
