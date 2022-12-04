fdb80a7e606fd725648c42780edda6f16cdfc12b
John Sanchez <johnmarion1126@gmail.com>
2022-12-04 19:54:28 +0000

    Add solution to day 4 part 2

--- 2022/day_4.rs
+++ 2022/day_4.rs
@@ -25,3 +25,26 @@ fn find_total_overlapping_pairs(input: &str) -> i32 {
 }
 
 //================================================================================================
+
+fn find_total_overlapping_pairs(input: &str) -> i32 {
+    let mut total = 0;
+
+    for i in input.lines() {
+        let sections = i.trim().split(",").collect::<Vec<&str>>();
+
+        let first_section_chars = sections[0].split("-").collect::<Vec<&str>>();
+        let second_section_chars = sections[1].split("-").collect::<Vec<&str>>();
+
+        let first_section_start: i32 = first_section_chars[0].parse().unwrap();
+        let first_section_end: i32 = first_section_chars[1].parse().unwrap();
+
+        let second_section_start: i32 = second_section_chars[0].parse().unwrap();
+        let second_section_end: i32 = second_section_chars[1].parse().unwrap();
+
+        if first_section_end >= second_section_start && second_section_end >= first_section_start {
+            total += 1;
+        }
+    }
+
+    total
+}
