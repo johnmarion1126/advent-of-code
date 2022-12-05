17401c6ce786f73a405bd9d1f1be825c18943d89
John Sanchez <johnmarion1126@gmail.com>
2022-12-05 18:32:23 +0000

    Add solution for day 5 part 2

--- 2022/day_5.rs
+++ 2022/day_5.rs
@@ -32,3 +32,49 @@ fn arrange_stacks(input: &str) -> String {
 
     result
 }
+
+//================================================================================================
+
+fn arrange_stacks(input: &str) -> String {
+    let mut result: String = "".to_string();
+
+    let mut stacks: Vec<Vec<char>> = vec![
+        // ['Z', 'N'].to_vec(),
+        // ['M', 'C', 'D'].to_vec(),
+        // ['P'].to_vec(),
+        ['G', 'T', 'R', 'W'].to_vec(),
+        ['G', 'C', 'H', 'P', 'M', 'S', 'V', 'W'].to_vec(),
+        ['C', 'L', 'T', 'S', 'G', 'M'].to_vec(),
+        ['J', 'H', 'D', 'M', 'W', 'R', 'F'].to_vec(),
+        ['P', 'Q', 'L', 'H', 'S', 'W', 'F', 'J'].to_vec(),
+        ['P', 'J', 'D', 'N', 'F', 'M', 'S'].to_vec(),
+        ['Z', 'B', 'D', 'F', 'G', 'C', 'S', 'J'].to_vec(),
+        ['R', 'T', 'B'].to_vec(),
+        ['H', 'N', 'W', 'L', 'C'].to_vec(),
+    ];
+
+    let moves = input
+        .lines()
+        .map(|x| x.trim().split(' ').collect::<Vec<&str>>())
+        .collect::<Vec<Vec<&str>>>();
+
+    for action in moves {
+        let amount: usize = action[1].parse::<i32>().unwrap().try_into().unwrap();
+        let from: usize = (action[3].parse::<i32>().unwrap() - 1).try_into().unwrap();
+        let to: usize = (action[5].parse::<i32>().unwrap() - 1).try_into().unwrap();
+
+        let stack_size = stacks[from].len();
+        let mut cargo = stacks[from]
+            .drain((stack_size - amount)..)
+            .collect::<Vec<char>>();
+        stacks[to].append(&mut cargo);
+    }
+
+    for stack in stacks {
+        if let Some(x) = stack.last() {
+            result.push(*x);
+        }
+    }
+
+    result
+}
