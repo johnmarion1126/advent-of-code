c53f3ddb27020d73f549647c297be7f068acb7ec
John Sanchez <johnmarion1126@gmail.com>
2022-12-11 03:29:15 +0000

    Add solution to day 10 part 1

---
+++ 2022/day_10.rs
@@ -0,0 +1,49 @@
+fn check_cycle(signal: i32, cycles: i32) -> i32 {
+    if cycles == 20
+        || cycles == 60
+        || cycles == 100
+        || cycles == 140
+        || cycles == 180
+        || cycles == 220
+    {
+        // println!("CYCLE {} SIGNAL {}", cycles, signal);
+        return signal * cycles;
+    }
+    0
+}
+
+fn get_total(input: &str) -> i32 {
+    let mut cycles = 0;
+    let mut total_signal = 0;
+    let mut signal = 1;
+
+    for i in input.lines() {
+        let parts: Vec<_> = i.trim().split(" ").collect();
+        let mut value = 0;
+
+        cycles += 1;
+        total_signal += check_cycle(signal, cycles);
+
+        match parts[..] {
+            ["addx", val] => {
+                // println!("ADDX");
+                value = val.parse::<i32>().unwrap();
+            }
+            ["noop"] => {
+                // println!("NOOP");
+                continue;
+            }
+            _ => {}
+        }
+
+        cycles += 1;
+        total_signal += check_cycle(signal, cycles);
+
+        signal += value;
+    }
+
+    total_signal
+}
+
+//================================================================================================
+
