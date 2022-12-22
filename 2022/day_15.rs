0de7fa0b31e595ae5a000ab7fcd8852209588974
John Sanchez <johnmarion1126@gmail.com>
2022-12-22 22:27:00 +0000

    Add solution for day 15 part 1

--- 2022/day_15.rs
+++ 2022/day_15.rs
@@ -1,7 +1,5 @@
 use std::collections::HashSet;
 
-use colored::*;
-
 #[derive(Debug)]
 struct Sensor {
     sensor_position: (i32, i32),
@@ -12,18 +10,17 @@ impl Sensor {
     fn new(sensor_position: (i32, i32), beacon_position: (i32, i32)) -> Self {
         Self {
             sensor_position,
-            beacon_position
+            beacon_position,
         }
     }
 
     fn manhattan_distance(&self) -> i32 {
-        (self.sensor_position.0 - self.beacon_position.0).abs() + (self.sensor_position.1 - self.beacon_position.1)
+        (self.sensor_position.0 - self.beacon_position.0).abs()
+            + (self.sensor_position.1 - self.beacon_position.1).abs()
     }
 }
 
-const SIZE: usize = 20;
-
-fn parse_input(input: &str) -> Option<Vec<Sensor>>{
+fn parse_input(input: &str) -> Option<Vec<Sensor>> {
     let mut sensors: Vec<Sensor> = Vec::new();
 
     for line in input.lines() {
@@ -37,86 +34,42 @@ fn parse_input(input: &str) -> Option<Vec<Sensor>>{
         let sensor_x_index = sensor_info.find("x=")?;
         let sensor_y_index = sensor_info.find(", y=")?;
 
-        let sensor_x = sensor_info[sensor_x_index+2..sensor_y_index].parse::<i32>().expect("Parsing Error");
-        let sensor_y = sensor_info[sensor_y_index+4..].parse::<i32>().expect("Parsing Error");
+        let sensor_x = sensor_info[sensor_x_index + 2..sensor_y_index]
+            .parse::<i32>()
+            .expect("Parsing Error");
+        let sensor_y = sensor_info[sensor_y_index + 4..]
+            .parse::<i32>()
+            .expect("Parsing Error");
 
         let beacon_info = parts[1];
         let beacon_x_index = beacon_info.find("x=")?;
         let beacon_y_index = beacon_info.find(", y=")?;
 
-        let beacon_x = beacon_info[beacon_x_index+2..beacon_y_index].parse::<i32>().expect("Parsing Error");
-        let beacon_y = beacon_info[beacon_y_index+4..].parse::<i32>().expect("Parsing Error");
+        let beacon_x = beacon_info[beacon_x_index + 2..beacon_y_index]
+            .parse::<i32>()
+            .expect("Parsing Error");
+        let beacon_y = beacon_info[beacon_y_index + 4..]
+            .parse::<i32>()
+            .expect("Parsing Error");
 
-        sensors.push(Sensor::new(
-            (sensor_x, sensor_y),
-            (beacon_x, beacon_y)
-        ));
+        sensors.push(Sensor::new((sensor_y, sensor_x), (beacon_y, beacon_x)));
     }
 
     Some(sensors)
 }
 
-fn print_matrix(matrix: &[[char; SIZE]; SIZE], target_row: i32) {
-    let mut picture: Vec<Vec<ColoredString>> = Vec::new();
-
-    for index in 0..matrix.len() {
-        let mut colored_values: Vec<ColoredString> = Vec::new();
-
-        for character in matrix[index] {
-            match character {
-                'S' => colored_values.push(character.to_string().truecolor(64, 145, 108)),
-                '#' => colored_values.push(character.to_string().truecolor(82, 183, 136)),
-                'B' => colored_values.push(character.to_string().truecolor(45, 106, 79)),
-                _ => {
-                    if index == target_row as usize {
-                        colored_values.push(character.to_string().truecolor(162, 210, 255));
-                    } else {
-                        colored_values.push(character.to_string().white());
-                    }
-                }
-            }
-        }
-
-        picture.push(colored_values);
-    }
-
-    for row in picture {
-        for value in row {
-            print!("{} ", value);
-        }
-        println!();
-    }
-}
-
 fn get_sensor_area(sensors: Vec<Sensor>, target_row: i32) -> usize {
-    let mut matrix = [['x'; SIZE]; SIZE];
     let mut positions: HashSet<i32> = HashSet::new();
-    let mut num_beacons_in_row = 0;
+    let mut beacons_in_row: HashSet<(i32, i32)> = HashSet::new();
 
     for sensor in sensors {
-        matrix[sensor.sensor_position.0 as usize][sensor.sensor_position.1 as usize] = 'S';
-        matrix[sensor.beacon_position.0 as usize][sensor.beacon_position.1 as usize] = 'B';
-
+        let y_distance = (target_row - sensor.sensor_position.0).abs();
         let distance = sensor.manhattan_distance();
 
-        // TOP
-        matrix[(sensor.sensor_position.0 - distance) as usize][sensor.sensor_position.1 as usize] = '#';
-        // BOTTOM
-        matrix[(sensor.sensor_position.0 + distance) as usize][sensor.sensor_position.1 as usize] = '#';
-
-        // LEFT
-        matrix[sensor.sensor_position.0 as usize][(sensor.sensor_position.1 - distance) as usize] = '#';
-        // RIGHT
-        matrix[sensor.sensor_position.0 as usize][(sensor.sensor_position.1 + distance) as usize] = '#';
-
-        let diameter = 1 + (distance * 2);
-        let y_distance = (target_row - sensor.sensor_position.0).abs();
-
-        let mut occupied_cells = (0, 0);
-
-        if target_row <= sensor.sensor_position.0 + distance 
-        && target_row >= sensor.sensor_position.0 - distance {
-            occupied_cells = (
+        if target_row <= sensor.sensor_position.0 + distance
+            && target_row >= sensor.sensor_position.0 - distance
+        {
+            let occupied_cells = (
                 (sensor.sensor_position.1 - distance) + y_distance,
                 (sensor.sensor_position.1 + distance) - y_distance,
             );
@@ -125,28 +78,47 @@ fn get_sensor_area(sensors: Vec<Sensor>, target_row: i32) -> usize {
                 positions.insert(num);
             }
 
-            if sensor.beacon_position.0 == target_row {
-                num_beacons_in_row += 1;
+            if sensor.beacon_position.0 == target_row
+                && !beacons_in_row.contains(&(sensor.beacon_position.0, sensor.beacon_position.1))
+            {
+                beacons_in_row.insert((sensor.beacon_position.0, sensor.beacon_position.1));
             }
         }
-
-
-        print_matrix(&matrix, target_row);
-        println!("Manhanttan Distance: {}", distance);
-        println!("Sensor Diameter: {}", diameter);
-        println!("Occupied Cells: {:?}", occupied_cells);
-
-        println!();
     }
 
-    positions.len() - num_beacons_in_row
+    positions.len() - beacons_in_row.len()
 }
 
 fn main() {
-    let mut sensors: Vec<Sensor> = Vec::new();
-    sensors.push(Sensor::new((4, 4), (5, 1)));
-    sensors.push(Sensor::new((8, 6), (7, 5)));
-    sensors.push(Sensor::new((14, 3), (13, 2)));
-    sensors.push(Sensor::new((6, 14), (8, 11)));
-    println!("Result: {}", get_sensor_area(sensors, 7));
+    let sensors = parse_input(
+        "Sensor at x=3907621, y=2895218: closest beacon is at x=3790542, y=2949630
+Sensor at x=1701067, y=3075142: closest beacon is at x=2275951, y=3717327
+Sensor at x=3532369, y=884718: closest beacon is at x=2733699, y=2000000
+Sensor at x=2362427, y=41763: closest beacon is at x=2999439, y=-958188
+Sensor at x=398408, y=3688691: closest beacon is at x=2275951, y=3717327
+Sensor at x=1727615, y=1744968: closest beacon is at x=2733699, y=2000000
+Sensor at x=2778183, y=3611924: closest beacon is at x=2275951, y=3717327
+Sensor at x=2452818, y=2533012: closest beacon is at x=2733699, y=2000000
+Sensor at x=88162, y=2057063: closest beacon is at x=-109096, y=390805
+Sensor at x=2985370, y=2315046: closest beacon is at x=2733699, y=2000000
+Sensor at x=2758780, y=3000106: closest beacon is at x=3279264, y=2775610
+Sensor at x=3501114, y=3193710: closest beacon is at x=3790542, y=2949630
+Sensor at x=313171, y=1016326: closest beacon is at x=-109096, y=390805
+Sensor at x=3997998, y=3576392: closest beacon is at x=3691556, y=3980872
+Sensor at x=84142, y=102550: closest beacon is at x=-109096, y=390805
+Sensor at x=3768533, y=3985372: closest beacon is at x=3691556, y=3980872
+Sensor at x=2999744, y=3998031: closest beacon is at x=3691556, y=3980872
+Sensor at x=3380504, y=2720962: closest beacon is at x=3279264, y=2775610
+Sensor at x=3357940, y=3730208: closest beacon is at x=3691556, y=3980872
+Sensor at x=1242851, y=838744: closest beacon is at x=-109096, y=390805
+Sensor at x=3991401, y=2367688: closest beacon is at x=3790542, y=2949630
+Sensor at x=3292286, y=2624894: closest beacon is at x=3279264, y=2775610
+Sensor at x=2194423, y=3990859: closest beacon is at x=2275951, y=3717327",
+    )
+    .unwrap();
+    let y = 2000000;
+
+    println!("Result: {}", get_sensor_area(sensors, y));
 }
+
+//================================================================================================
